b381c394570e2cc74fd364f36489f24d3423efe1
Bernd Pichon <pichon@hm.edu>
2016-04-10 15:05:55 +0200

    Chunked implementiert. Die Requests werden nun in kleinerene HubResponses verschickt.

--- src/protocols/CPResponse.java
+++ src/protocols/CPResponse.java
@@ -9,17 +9,21 @@ import java.io.IOException;
 public class CPResponse {
 
     int requestId;
-    int part;
+    int requestPart;
+    int responsePart;
+    boolean isLast;
     byte[] content;
 
-    public CPResponse(int requestId, int part, byte[] content) {
+    public CPResponse(int requestId, int requestPart, int responsePart, boolean isLast, byte[] content) {
         this.requestId = requestId;
-        this.part = part;
+        this.requestPart = requestPart;
+        this.responsePart = responsePart;
+        this.isLast = isLast;
         this.content = content;
     }
 
     public byte[] getBytes() {
-        /* Header-Line extrahieren. PR <length> <id> <part> \r\n bytes */
+        /* Header-Line extrahieren. PR <length> <id> <requestPart> <responsePart> <isLast> \r\n bytes */
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
             out.write("PR".getBytes("US-ASCII"));
@@ -28,12 +32,17 @@ public class CPResponse {
             out.write(32);  //Space
             out.write(("" + requestId).getBytes("US-ASCII"));
             out.write(32);  //Space
-            out.write(("" + part).getBytes("US-ASCII"));
+            out.write(("" + requestPart).getBytes("US-ASCII"));
+            out.write(32);  //Space
+            out.write(("" + responsePart).getBytes("US-ASCII"));
+            out.write(32);  //Space
+            out.write((isLast ? "t" : "f").getBytes("US-ASCII"));
             out.write(13);  //\r
             out.write(10);  //\n
             out.write(content);
         } catch (IOException e) {
             e.printStackTrace();
+            // Ignore, unwichtig
         }
         return out.toByteArray();
     }
